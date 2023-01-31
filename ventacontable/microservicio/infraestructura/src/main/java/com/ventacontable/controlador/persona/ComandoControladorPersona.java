8d2038951b303569d4631eec55a0bee4c2ec46ae
juandavid748 <jsaroca@bancolombia.com.co>
2023-01-30 20:11:54 -0500

    se elimina import no usado

--- ventacontable/microservicio/infraestructura/src/main/java/com/ventacontable/controlador/persona/ComandoControladorPersona.java
+++ ventacontable/microservicio/infraestructura/src/main/java/com/ventacontable/controlador/persona/ComandoControladorPersona.java
@@ -5,8 +5,6 @@ import com.ventacontable.comando.persona.ComandoPersona;
 import com.ventacontable.comando.persona.manejador.ManejadorCrearPersona;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.logging.Logger;
-
 @RestController
 @RequestMapping("/persona")
 public class ComandoControladorPersona {
